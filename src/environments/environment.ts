f0b3dd70b3d65723d2e5e824475b06442797849e
fausto_montiel <fmontiel@ewaysa.com>
2020-04-18 04:18:31 -0300

    Primer commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDKsNfqiOt8KLS2SrRWDtVYjo0uq3aQjLg",
+    authDomain: "alarmaderobo-af677.firebaseapp.com",
+    databaseURL: "https://alarmaderobo-af677.firebaseio.com",
+    projectId: "alarmaderobo-af677",
+    storageBucket: "alarmaderobo-af677.appspot.com",
+    messagingSenderId: "440439067483",
+    appId: "1:440439067483:web:982e98ea2ee0594100007e"
+  }
 };
 
 /*
